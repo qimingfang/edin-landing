4652659dc44f1a2c6b153a401fc73bf851a04d94
Qiming Fang <qf26@cornell.edu>
2021-12-17 16:18:26 -0500

    chore: build

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import './App.css';
-
-function MouseScoll () {
-  return (
-    <div className="mouse">
-      <div className="scroll"></div>
-    </div>
-  )
-}
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="container-fluid">
         <section className="box-intro">
-            <div className="table-cell">
-                <h1 className="box-headline letters rotate-2">
-                    <span className="box-words-wrapper">
-                        <b className="is-visible">&nbsp;music.</b>
-                        <b>&nbsp;design.</b>
-                        <b>&nbsp;tech.</b>
-                    </span>
+          <div className="table-cell">
+            <h1 className="box-headline letters rotate-2">
+              <span className="box-words-wrapper">
+                <b className="is-visible">&nbsp;music.</b>
+                <b>&nbsp;design.</b>
+                <b>&nbsp;tech.</b>
+              </span>
             </h1>
-                <h5>welcome to classical effect</h5>
-            </div>
+            <h5>welcome to classical effect</h5>
+          </div>
         </section>
       </div>
     </>
